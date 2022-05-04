e1b387a112b031dd075fbf38e1729c0ad5258e33
Daniel Petkov <sondbg@abv.bg>
2022-05-04 22:04:17 +0300

    Add files via upload

---
+++ 6.Promotions.js
@@ -0,0 +1,27 @@
+function ticketSale(type,age){
+    if(age>=0 && age <=18){
+        switch (type) {
+            case 'Weekday': console.log('12$');   break;
+            case 'Weekend': console.log('15$');   break;
+            case 'Holiday': console.log('5$');    break;
+            default:        console.log('Error!');break;
+        }
+    }else if(age>18 && age <=64){
+        switch (type) {
+            case 'Weekday': console.log('18$');   break;
+            case 'Weekend': console.log('20$');   break;
+            case 'Holiday': console.log('12$');    break;
+            default:        console.log('Error!');break;
+        }
+    }else if(age>64 && age <=122){
+        switch (type) {
+            case 'Weekday': console.log('12$');   break;
+            case 'Weekend': console.log('15$');   break;
+            case 'Holiday': console.log('10$');    break;
+            default:        console.log('Error!');break;
+        }
+    }else{
+        console.log('Error!');
+    }
+}
+ticketSale('Weekfffday',42)
